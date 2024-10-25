96f22af78f5dcb46ff2060b1af941e0867f5a4f4
Cyril Romero <cyril.romero@etu.unilim.fr>
2024-10-25 15:21:00 +0200

    ajout de l'affichage de catégorie sur le classement, ajout de la vue des catégories des sons quand on clique sur eux de plus acces a la nouvelle navigation nommé categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\View\View;
+
+class CategoryController extends Controller
+{
+    public function index(): View
+    {
+        $categories = Category::withCount('tracks')->orderBy('name')->get();
+        return view('app.categories.index', compact('categories'));
+    }
+
+    public function show(Category $category): View
+    {
+        $tracks = $category->tracks()->with('week')->paginate(20);
+        return view('app.categories.show', compact('category', 'tracks'));
+    }
+}
